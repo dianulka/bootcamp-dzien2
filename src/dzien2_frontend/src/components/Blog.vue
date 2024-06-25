2e2ff5b91af304624029ef71c8a1995a6e80fe25
dianulka <diana332m@gmail.com>
2024-06-25 19:56:37 +0200

    added functionality to add and list blog posts

--- src/dzien2_frontend/src/components/Blog.vue
+++ src/dzien2_frontend/src/components/Blog.vue
@@ -3,6 +3,9 @@
         <button @click="pobierzWpisy">refresh</button>
         Cześć Blog! ^__^
         {{ wpisy }}
+        <input v-model="nowyBlog" type="text">
+        <button @click="dodajWpis">dodaj</button>
+
     </div>
 
 </template>
@@ -12,13 +15,16 @@ import { dzien2_backend } from "declarations/dzien2_backend/index"
 export default {
     data() {
         return {
-            wpisy : [] // lista
-
+            wpisy : [], // lista
+            nowyBlog: ""
         }
     },
     methods: {
         async pobierzWpisy() {
             this.wpisy = await dzien2_backend.odczytaj_wpisy()
+        },
+        async dodajWpis() {
+            await dzien2_backend.dodaj_wpis(this.nowyBlog);
         }
     }
 }
